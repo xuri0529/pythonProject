4494e450eb2f596804c1bc7e393d7acb732ec966
owen0529 <owen0529@sina.com>
2023-02-16 17:40:11 +0800

    first test

--- 0214001.py
+++ 0214001.py
@@ -12,26 +12,26 @@ from datetime import timedelta
 
 default_args = {
     'owner': 'jifeng.si',
-    'depends_on_past': False,
+    'depends_on_past': False,   # True时，表示只有当上一个task成功时，当前task才能启动
     'start_date': airflow.utils.dates.days_ago(2),
-    'email': ['1203745031@qq.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5)
+    'email': ['1203745031@qq.com'], # 可以配置一个邮件列表，触发邮件发送时将向列表中的邮箱发送对应邮件
+    'email_on_failure': False,  # task失败时，是否触发邮件发送
+    'email_on_retry': False,    # 重试时是否触发邮件发送
+    'retries': 1,   # 失败重试次数
+    'retry_delay': timedelta(minutes=5) # 失败重试时，之间的时间间隔
 }
 
 # -------------------------------------------------------------------------------
 # dag
 
 dag = DAG(
-    'example_hello_world_dag',
-    default_args=default_args,
-    description='my first DAG',
-    schedule_interval=timedelta(days=1))
+    dag_id='example_hello_world_dag',   # 唯一标识
+    default_args=default_args,  # 默认参数
+    description='my first DAG', # 描述
+    schedule_interval=timedelta(days=1))    # 调度时间
 
 # -------------------------------------------------------------------------------
-# first operator 打印日期
+# first operator 打印日期，BashOperator用来执行Bash脚本
 
 date_operator = BashOperator(
     task_id='date_task',
@@ -43,13 +43,13 @@ date_operator = BashOperator(
 
 sleep_operator = BashOperator(
     task_id='sleep_task',
-    depends_on_past=False,
+    depends_on_past=False,  # True时，表示只有当上一个task成功时，当前task才能启动
     bash_command='sleep 5',
     dag=dag)
 
 
 # -------------------------------------------------------------------------------
-# third operator 在执行任务时调用print_hello函数
+# third operator 在执行任务时调用print_hello函数， PythonOperator用来调用Python函数
 
 def print_hello():
     return 'Hello world!'
