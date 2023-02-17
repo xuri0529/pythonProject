e9757c33992b1ff8ffa5e246509b4dccddf934d1
owen0529 <owen0529@sina.com>
2023-02-17 15:11:48 +0800

    0217003添加修改语句描述

--- 0214001.py
+++ 0214001.py
@@ -18,17 +18,19 @@ default_args = {
     'email_on_failure': False,  # task失败时，是否触发邮件发送
     'email_on_retry': False,    # 重试时是否触发邮件发送
     'retries': 1,   # 失败重试次数
-    'retry_delay': timedelta(minutes=5) # 失败重试时，之间的时间间隔
+    'retry_delay': timedelta(minutes=5) # 失败重试时的时间间隔
 }
 
 # -------------------------------------------------------------------------------
 # dag   实例化DAG对象来进行任务
 
 dag = DAG(
-    dag_id='example_hello_world_dag',   # 唯一标识
-    default_args=default_args,  # 默认参数
+    dag_id='example_hello_world_dag',   # 唯一标识， 必须完全由字母、数字、下划线组成
+    default_args=default_args,  # 外部定义的 dic 格式的参数
     description='my first DAG', # 描述
-    schedule_interval=timedelta(days=1))    # 调度时间
+    schedule_interval=timedelta(days=1) # 调度时间
+    catchup=False  # 执行DAG时，将开始时间到目前所有该执行的任务都执行，默认为True
+    )
 
 # -------------------------------------------------------------------------------
 # first operator 打印日期，BashOperator用来执行Bash脚本
@@ -63,5 +65,12 @@ hello_operator = PythonOperator(
 # -------------------------------------------------------------------------------
 # dependencies
 
-sleep_operator.set_upstream(date_operator)
+sleep_operator.set_upstream(date_operator)  # sleep_operator会在date_operator执行完成之后执行
 hello_operator.set_upstream(date_operator)
+
+
+"""
+catchup可以在airflow配置文件airflow.cfg的scheduler下，设置catchup_by_default=True（默认）或False，这个设置是全局性的设置；
+将以上python配置文件上传到airflowhome/dags目录下，默认AIRFLOW_HOME为安装节点的/root/airflow目录，
+该文件每修改一次，需要重新上传一次，并重启airflow，DAG执行调度
+"""
