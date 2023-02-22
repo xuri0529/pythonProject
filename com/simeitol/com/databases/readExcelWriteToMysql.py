20281a79633cbedbb177b8f6940897547931b9bf
owen0529 <owen0529@sina.com>
2023-02-22 16:51:46 +0800

    0222 修改：dataframe类型的fillna函数备注完善

--- com/simeitol/com/databases/readExcelWriteToMysql.py
+++ com/simeitol/com/databases/readExcelWriteToMysql.py
@@ -15,7 +15,7 @@ def getSheets(path, sheet_name=None):
 def getSheetDatas(sheet_name):
     # header 指定作为列名的行，默认0，即取第一行的值为列名；若数据不包含列名，则设定 header = None
     datas = pd.read_excel(path, sheet_name=sheet_name, header=3)
-    ## 在pandas中空值以nan展示，当写入数据库时的空值需要转换为None
+    ## 在pandas中空值以nan展示，当写入数据库时的空值需要转换为None,使用缺失数据填充函数fillna的指定值进行填充
     datas = datas.fillna(value='None')
     # 获取所有列dataframe类型的columns和keys()返回内容类型都是index类型数据，可以直接转list、tuple，或者使用values获得列表类型的值(下面2行语句返回相同)
     columns = datas.keys().values
@@ -102,6 +102,7 @@ if __name__ == '__main__':
 
     sheet_names = getSheets(path, sheet_name=None)
 
+    # 按照sheet逐个进行数据的读取和表的写入
     for sheet_name in sheet_names:
         columns, datas = getSheetDatas(sheet_name)
 
